facb9f6ca125edf28de2148004ab9c16632c72ff
June Roh <texasroh@gmail.com>
2022-10-31 00:18:56 -0400

    add useForm on product upload

--- components/textarea.tsx
+++ components/textarea.tsx
@@ -1,26 +1,35 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+
 interface TextAreaProps {
-    label?: string;
-    name?: string;
-    [key: string]: any;
+  register: UseFormRegisterReturn;
+  label?: string;
+  name?: string;
+  [key: string]: any;
 }
 
-export default function TextArea({ label, name, ...rest }: TextAreaProps) {
-    return (
-        <div>
-            {label ? (
-                <label
-                    htmlFor={name}
-                    className="mb-1 block text-sm font-medium text-gray-700"
-                >
-                    {label}
-                </label>
-            ) : null}
-            <textarea
-                id={name}
-                className="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500 "
-                rows={4}
-                {...rest}
-            />
-        </div>
-    );
+export default function TextArea({
+  label,
+  name,
+  register,
+  ...rest
+}: TextAreaProps) {
+  return (
+    <div>
+      {label ? (
+        <label
+          htmlFor={name}
+          className="mb-1 block text-sm font-medium text-gray-700"
+        >
+          {label}
+        </label>
+      ) : null}
+      <textarea
+        {...register}
+        id={name}
+        className="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-orange-500 "
+        rows={4}
+        {...rest}
+      />
+    </div>
+  );
 }
