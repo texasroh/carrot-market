e4ce34bf19b09d14a619102a626ce18b168ca85a
June Roh <june.roh@streetsmarket.com>
2022-10-31 11:08:31 -0400

    show fav counts at the index

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,15 @@ import Head from "next/head";
 import useSWR from "swr";
 import { Product } from "@prisma/client";
 
+interface ProductWithFavCount extends Product {
+    _count: {
+        favorites: number;
+    };
+}
+
 interface ProductsResponse {
     ok: boolean;
-    products: Product[];
+    products: ProductWithFavCount[];
 }
 
 const Home: NextPage = () => {
@@ -28,7 +34,7 @@ const Home: NextPage = () => {
                         title={product.name}
                         price={product.price}
                         comments={1}
-                        hearts={1}
+                        hearts={product._count.favorites}
                     />
                 ))}
                 <FloatingButton href="/products/upload">
