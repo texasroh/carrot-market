32993e973e9970b478b2852ed9c03ff8bb339e86
June Roh <texasroh@gmail.com>
2022-11-01 01:04:06 -0400

    add geolocation for post

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -4,51 +4,62 @@ import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    body: { question, latitude, longitude },
+    session: { user },
+    query,
+  } = req;
+  if (req.method === "POST") {
+    const post = await client.post.create({
+      data: {
+        question,
+        latitude,
+        longitude,
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+      },
+    });
+    res.json({ ok: true, post });
+  } else if (req.method === "GET") {
     const {
-        body: { question, latitude, longitude },
-        session: { user },
-        query,
+      query: { latitude, longitude },
     } = req;
-    console.log(query);
-    if (req.method === "POST") {
-        const post = await client.post.create({
-            data: {
-                question,
-                latitude,
-                longitude,
-                user: {
-                    connect: {
-                        id: user?.id,
-                    },
-                },
-            },
-        });
-        res.json({ ok: true, post });
-    } else if (req.method === "GET") {
-        const {
-            query: { latitude, longitude },
-        } = req;
-        const posts = await client.post.findMany({
-            include: {
-                user: {
-                    select: {
-                        id: true,
-                        name: true,
-                        avatar: true,
-                    },
-                },
-                _count: {
-                    select: {
-                        wonderings: true,
-                        answers: true,
-                    },
-                },
-            },
-        });
-        res.json({ ok: true, posts });
-    }
+    const parsedLatitude = parseFloat(latitude?.toString() || "");
+    const parsedLongitude = parseFloat(longitude?.toString() || "");
+    const posts = await client.post.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            wonderings: true,
+            answers: true,
+          },
+        },
+      },
+      where: {
+        latitude: {
+          lte: parsedLatitude + 1,
+          gte: parsedLatitude - 1,
+        },
+        longitude: {
+          lte: parsedLongitude + 1,
+          gte: parsedLongitude - 1,
+        },
+      },
+    });
+    res.json({ ok: true, posts });
+  }
 }
 
 export default withApiSession(
-    withHandler({ methods: ["POST", "GET"], handler })
+  withHandler({ methods: ["POST", "GET"], handler })
 );
