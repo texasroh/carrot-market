c9c4168ed441d5e2a00519ef090c5644841bf3a2
June Roh <june.roh@streetsmarket.com>
2022-11-01 16:38:01 -0400

    misc

---
+++ pages/api/streams/[id].ts
@@ -0,0 +1,22 @@
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withApiSession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+
+async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseType>
+) {
+    const {
+        query: { id },
+    } = req;
+
+    const stream = await client.stream.findUnique({
+        where: {
+            id: +id!.toString(),
+        },
+    });
+    res.json({ ok: true, stream });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
