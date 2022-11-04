ac6cad35672d04563ecde406e3ba345bced5d576
June Roh <june.roh@streetsmarket.com>
2022-11-04 14:52:52 -0400

    add _document

---
+++ pages/_document.tsx
@@ -0,0 +1,46 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+class CustomDocument extends Document {
+    render(): JSX.Element {
+        return (
+            <Html lang="ko">
+                <Head>
+                    {/* 
+                        구글 폰트만 사용할것
+                        nextjs는 구글폰트를 빌드시 최적화해줌.
+                    */}
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+                {/*
+                next/Script는 3가지 strategy가 있다. afterInteractive(default) / beforeInteractive / lazyOnload
+                Script는 _app.tsx로 가도되고 _document.tsx로 가도 된다.
+                */}
+                <Script
+                    src="https://developers.kaka.com/sdk/js/kakao.js"
+                    strategy="lazyOnload"
+                />
+                <Script
+                    src="https://connect.facebook.net/en_US/sdk.js"
+                    onLoad={() => {
+                        window.fbAsyncInit = function () {
+                            FB.init({
+                                appId: "your-app-id",
+                                autoLogAppEvents: true,
+                                xfbml: true,
+                                version: "v13.0",
+                            });
+                        };
+                    }}
+                />
+
+                <Script />
+            </Html>
+        );
+    }
+}
+
+export default CustomDocument;
