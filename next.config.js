4907a57f710dbdd217520517ceb993aad0b5b664
June Roh <june.roh@streetsmarket.com>
2022-11-07 16:30:07 -0500

    ㅁㅇㅇ ㄴㄷㄱㅍㄷㄱ 채ㅡㅔㅐㅜ둣ㄴ

--- next.config.js
+++ next.config.js
@@ -3,15 +3,22 @@
 const path = require("path");
 
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ["imagedelivery.net", "videodelivery.net"],
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-    // prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`,   // 두 파일에서 선언한 변수를 모든 파일에서 사용할 수 있다.
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ["imagedelivery.net", "videodelivery.net"],
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, "styles")],
+        // prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`,   // 두 파일에서 선언한 변수를 모든 파일에서 사용할 수 있다.
+    },
+    // experimental: {    // server Components를 위한 옵션
+    //     reactRoot: true,
+    //     runtime: "nodejs",
+    //     concurrentFeatures: true,
+    //     serverComponents: true,
+    //     appDir: "./pages",
+    // },
 };
 
 module.exports = nextConfig;
---
+++ pages/coins.server.tsx
@@ -0,0 +1,7 @@
+export default () => {
+    return (
+        <div>
+            <h1>Welcome to RSC</h1>
+        </div>
+    );
+};
